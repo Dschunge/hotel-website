a826247f2f47c575355857002aa277f3b3f6de5c
Dschunge <schaar.juergen@gmail.com>
2024-01-26 07:47:43 -0600

    second commit

--- src/components/Home-4/TestimonialSec.tsx
+++ src/components/Home-4/TestimonialSec.tsx
@@ -42,7 +42,7 @@ const TestimonialSec = ({pb}:classType) => {
             >
               <div className="col-lg-9">
                 <div className="bd-section__title-wrapper is-white ">
-                  <p className="bd-section__subtitle mb-20">testimonials</p>
+                  <p className="bd-section__subtitle mb-20">Testimonials</p>
                   <h2 className=" bd-section__title mb-40 mbs-10">
                     What our customers
                     <br />
@@ -63,64 +63,7 @@ const TestimonialSec = ({pb}:classType) => {
           <div className="col-12 wow fadeInUp" data-wow-delay=".5s">
             <div className="bd-testimonial-2__slider p-relative ryl-slider2-pagination ADASD">
               <div className="bd-testimonial-2-active">
-                <Slider {...settings}>
-                  {testimonials?.map((item) => (
-                    <div key={item.id} className="bd-testimonial-2">
-                      <div className="row justify-content-between align-items-center">
-                        <div className="col-xl-6 col-lg-7">
-                          <div className="bd-testimonial-2__content">
-                            <div className="bd-testimonial-2__quote mb-30">
-                              <Image
-                                src={item.icon}
-                                style={{ width: "auto", height: "auto" }}
-                                alt="image not found"
-                              />
-                            </div>
-                            <p> {item.details} </p>
-                            <hr />
-                            <div className="bd-testimonial-2__author d-flex justify-content-between align-items-sm-center">
-                              <div className="bd-testimonial-2__thumb-wrap d-flex align-items-sm-center flex-column gap-3">
-                                <div className="bd-testimonial-2__thumb-2 d-lg-none">
-                                  <Image
-                                    src={item.img}
-                                    style={{ width: "100%", height: "100%" }}
-                                    alt="image not found"
-                                  />
-                                </div>
-                                <div className="bd-testimonial-2__title-wrap">
-                                  <h3 className="bd-testimonial-2__title">
-                                    
-                                    {item.title}
-                                  </h3>
-                                  <span className="bd-testimonial-2__des">
-                                    
-                                    {item.subTitle}
-                                  </span>
-                                </div>
-                              </div>
-                              <div className="bd-testimonial-2__rating d-flex">
-                                <i className="fa-solid fa-star"></i>
-                                <i className="fa-solid fa-star"></i>
-                                <i className="fa-solid fa-star"></i>
-                                <i className="fa-solid fa-star"></i>
-                                <i className="fa-solid fa-star"></i>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <div className="col-xl-4 col-lg-4">
-                          <div className="bd-testimonial-2__thumb d-none d-lg-block">
-                            <Image
-                              src={item.img}
-                              style={{ width: "100%", height: "100%" }}
-                              alt="image not found"
-                            />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  ))}
-                </Slider>
+            
               </div>
             </div>
           </div>
